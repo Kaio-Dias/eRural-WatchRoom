691445a58f02fcc6d1b2e9ef83cb4b737bfbb570
Kaio Dias <kaiodiask12@gmail.com>
2025-10-21 16:06:09 -0300

    feat(cable): identifica cliente com cookie assinado para presença/logs

--- backend/app/channels/application_cable/connection.rb
+++ backend/app/channels/application_cable/connection.rb
@@ -1,4 +1,15 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :client_id
+
+    def connect
+      self.client_id = find_or_set_client_id
+    end
+
+    private
+
+    def find_or_set_client_id
+      cookies.signed[:client_id] ||= SecureRandom.uuid
+    end
   end
 end
